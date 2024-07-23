aaa1e17e97fe260378ee7b8e7d93acc3b2890aea
bizackk <chozito0205@gmail.com>
2024-07-23 17:24:36 -0500

    vitalbot

---
+++ routes/rutasAPI.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter,HTTPException
+from sqlalchemy import select
+
+from models.chatbot import chat
+from database.basedatos import conexion
+
+rutas=APIRouter()
+
+@rutas.get("/vitalbot")
+def consultarChat():
+    try:
+        consulta=select(chat)
+        resultado=conexion.execute(consulta).fetchall()
+        resultadoJSON=[{'id':row.id,'pregunta':row.pregunta,'respuesta':row.respuestas} for row in resultado]
+        return resultadoJSON
+    
+    except Exception as error:
+        raise HTTPException(status_code=500,detail=str(error))
+            
